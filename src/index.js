81e8ecb164995a4241774610922043a0cdf667e3
Daniel Rodríguez <sadasant@gmail.com>
2017-12-06 21:24:51 +0000

    removed things

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
-import 'core-js/es6/symbol'
 import React from 'react'
 import ReactDOM from 'react-dom'
-import '@highweb/bootstrap-kit/dist/bootstrap-kit.css'
 import MovieResults from './components/MovieResults'
 
 ReactDOM.render(
