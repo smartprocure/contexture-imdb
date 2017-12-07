eeb147af4405786bbe65a2a1b5fff3f0b35433d9
Decrapifier <drodriguez@smartprocure.us>
2017-12-07 21:00:37 +0000

    Automagically formatted by Duti!
    https://github.com/smartprocure/duti

--- src/utils/mobxSearchTree.js
+++ src/utils/mobxSearchTree.js
@@ -1,9 +1,9 @@
-import * as contextureClient from "contexture-client";
-import { toJS, extendObservable } from "mobx";
+import * as contextureClient from 'contexture-client'
+import { toJS, extendObservable } from 'mobx'
 
 export default (tree, service) =>
   contextureClient.ContextTree(tree, service, undefined, {
     // debug: true,
     snapshot: toJS,
-    extend: extendObservable
-  });
+    extend: extendObservable,
+  })
--- src/utils/searchService.js
+++ src/utils/searchService.js
@@ -1,28 +1,28 @@
-import _ from "lodash/fp";
-import Contexture from "contexture";
-import elasticsearch from "elasticsearch-browser";
-import contextureES from "contexture-elasticsearch";
-import contextureESTypes from "contexture-elasticsearch/src/types";
+import _ from 'lodash/fp'
+import Contexture from 'contexture'
+import elasticsearch from 'elasticsearch-browser'
+import contextureES from 'contexture-elasticsearch'
+import contextureESTypes from 'contexture-elasticsearch/src/types'
 
 export default Contexture({
   schemas: {
     imdb: {
       elasticsearch: {
-        index: "movies",
-        type: "movie"
-      }
-    }
+        index: 'movies',
+        type: 'movie',
+      },
+    },
   },
   providers: {
     elasticsearch: contextureES({
       getClient: _.memoize(() =>
         elasticsearch.Client({
-          apiVersion: "6.0",
+          apiVersion: '6.0',
           host:
-            "https://y85ukgvi1w:4s1cvayng9@first-cluster-5089088915.us-east-1.bonsaisearch.net"
+            'https://y85ukgvi1w:4s1cvayng9@first-cluster-5089088915.us-east-1.bonsaisearch.net',
         })
       ),
-      types: contextureESTypes()
-    })
-  }
-});
+      types: contextureESTypes(),
+    }),
+  },
+})
