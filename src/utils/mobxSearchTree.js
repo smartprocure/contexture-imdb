c2b22a1dff2cc364faa71c725203df0290ab5fde
Decrapifier <drodriguez@smartprocure.us>
2018-01-31 23:08:33 +0000

    Automagically formatted by Duti!
    https://github.com/smartprocure/duti

--- src/utils/mobxSearchTree.js
+++ src/utils/mobxSearchTree.js
@@ -2,9 +2,12 @@ import * as contextureClient from 'contexture-client'
 import { toJS, extendObservable } from 'mobx'
 
 export default (tree, service) =>
-  contextureClient.ContextTree({
-    // debug: true,
-    snapshot: toJS,
-    extend: extendObservable,
-    service
-  }, tree)
+  contextureClient.ContextTree(
+    {
+      // debug: true,
+      snapshot: toJS,
+      extend: extendObservable,
+      service,
+    },
+    tree
+  )
