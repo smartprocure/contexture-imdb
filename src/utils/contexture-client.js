fa248a067b94dc368b08178365789869c71c3009
Samuel Greene <sgreene@smartprocure.us>
2018-02-27 21:15:19 +0100

    add contexture-client from talk (duplicative for now)

---
+++ src/utils/contexture-client.js
@@ -0,0 +1,17 @@
+import _ from 'lodash/fp'
+import {ContextTree, exampleTypes} from 'contexture-client'
+import {observable, toJS, extendObservable} from 'mobx'
+import service from './searchService'
+
+export default _.flow(
+  observable,
+  ContextTree({
+    service: async dto => ({
+      data: await service(dto),
+    }),
+    types: exampleTypes,
+    // debug: true,
+    snapshot: toJS,
+    extend: extendObservable,
+  })
+)
