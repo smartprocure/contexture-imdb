3e41515d43fd684ffadcddf54d6bc286bee026d6
Samuel Greene <sgreene@smartprocure.us>
2018-03-15 04:46:52 +0100

    split stories to separate files

---
+++ src/stories/advanced.js
@@ -0,0 +1,66 @@
+import React from 'react'
+import {Provider} from 'mobx-react'
+import {storiesOf} from '@storybook/react'
+import SearchRoot from 'contexture-react/dist/components/SearchRoot'
+import Types from 'contexture-react/dist/exampleTypes'
+import {ResultCount} from 'contexture-react/dist/example-types/components'
+import Contexture from '../utils/contexture-client'
+import ResultTable from '../components/ResultTable'
+import IMDBCards from '../components/IMDBCards'
+
+let tree = Contexture({
+  key: 'root',
+  type: 'group',
+  join: 'and',
+  schema: 'imdb',
+  children: [
+    {
+      key: 'searchRoot',
+      type: 'group',
+      join: 'and',
+      children: [
+        {
+          key: 'searchQuery',
+          type: 'query',
+          field: 'title',
+          query: 'rabbit',
+        },
+        {
+          key: 'searchFacet',
+          type: 'facet',
+          field: 'genres.keyword',
+          fieldMode: 'word',
+          context: {
+            options: [],
+          },
+        },
+      ],
+    },
+    {
+      key: 'results',
+      type: 'results',
+      pageSize: 10,
+      page: 1,
+      context: {
+        response: {
+          results: [],
+          totalRecords: null,
+        },
+      },
+    },
+  ],
+})
+export default () => (
+  <div>
+    <SearchRoot tree={tree} types={Types} path={['root', 'searchRoot']} />
+    <Provider tree={tree}>
+      <div>
+        <h1>
+          <ResultCount path={['root', 'results']} />
+        </h1>
+        <ResultTable path={['root', 'results']} />
+      </div>
+    </Provider>
+    <pre>{JSON.stringify(tree, null, 2)}</pre>
+  </div>
+)
---
+++ src/stories/basic.js
@@ -0,0 +1,115 @@
+import React from 'react'
+import {Provider} from 'mobx-react'
+import {storiesOf} from '@storybook/react'
+import Contexture from '../utils/contexture-client'
+import {
+  Facet,
+  Range,
+  Query,
+  ResultCount,
+  DateHistogram,
+} from 'contexture-react/dist/example-types/components'
+import {Flex} from 'contexture-react/dist/example-types/Flex'
+import SpacedList from 'contexture-react/dist/example-types/SpacedList'
+import IMDBCards from '../components/IMDBCards'
+
+let formatYear = x => new Date(x).getFullYear() + 1
+
+let tree = Contexture({
+  key: 'searchRoot',
+  type: 'group',
+  join: 'and',
+  schema: 'imdb',
+  children: [
+    {
+      key: 'searchQuery',
+      type: 'query',
+      field: 'title',
+      query: '',
+    },
+    {
+      key: 'searchFacet',
+      type: 'facet',
+      field: 'genres.keyword',
+      fieldMode: 'word',
+      optionsFilter: '',
+      context: {
+        options: [],
+      },
+    },
+    {
+      key: 'searchActors',
+      type: 'facet',
+      field: 'actors.keyword',
+      fieldMode: 'word',
+      context: {
+        options: [],
+      },
+    },
+    {
+      key: 'searchRange',
+      type: 'number',
+      field: 'metaScore',
+      min: 0,
+      max: 100,
+    },
+    {
+      key: 'results',
+      type: 'results',
+      pageSize: 6,
+      page: 1,
+      context: {
+        response: {
+          results: [],
+          totalRecords: null,
+        },
+      },
+    },
+    {
+      key: 'releases',
+      type: 'dateHistogram',
+      key_field: 'released',
+      value_field: 'imdbVotes',
+      interval: '3650d',
+      context: {
+        entries: [],
+        maxDate: null,
+        minDate: null,
+      },
+    },
+  ],
+})
+
+export default () => (
+  <Provider tree={tree}>
+    <SpacedList>
+      <Query path={['searchRoot', 'searchQuery']} />
+      <Flex>
+        <div style={{flex: 1}}>
+          <SpacedList>
+            <div>
+              <b>MetaScore</b>
+              <Range path={['searchRoot', 'searchRange']} />
+            </div>
+            <div>
+              <b>Genre</b>
+              <Facet path={['searchRoot', 'searchFacet']} />
+            </div>
+            <div>
+              <b>Actors</b>
+              <Facet path={['searchRoot', 'searchActors']} />
+            </div>
+          </SpacedList>
+        </div>
+        <div style={{flex: 4}}>
+          <ResultCount path={['searchRoot', 'results']} />
+          <DateHistogram
+            path={['searchRoot', 'releases']}
+            format={formatYear}
+          />
+          <IMDBCards path={['searchRoot', 'results']} />
+        </div>
+      </Flex>
+    </SpacedList>
+  </Provider>
+)
