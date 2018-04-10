99c60933487e11bf6a1af6a83bdb60ba6888837a
Decrapifier <drodriguez@smartprocure.us>
2018-04-10 21:24:40 +0000

    Automagically formatted by Duti!
    https://github.com/smartprocure/duti

--- src/utils/contexture.js
+++ src/utils/contexture.js
@@ -33,14 +33,14 @@ export default _.flow(
               apiVersion: '6.0',
               host:
                 // 'https://y85ukgvi1w:4s1cvayng9@first-cluster-5089088915.us-east-1.bonsaisearch.net',
-                'localhost:9200'
-                // Won't work until we enable SSL
-                // 'http://35.163.200.173:9200/',
+                'localhost:9200',
+              // Won't work until we enable SSL
+              // 'http://35.163.200.173:9200/',
             })
           ),
           types: contextureESTypes(),
         }),
       },
-    })
+    }),
   })
 )
