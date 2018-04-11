7e60accd9d2afd2f11536058db498bd0fd1529bc
Samuel Greene <sgreene@smartprocure.us>
2018-04-11 22:49:22 +0200

    shiny new result table with schema inference and overrides

--- src/stories/filterList.js
+++ src/stories/filterList.js
@@ -93,7 +93,39 @@ export default () => (
           <h1>
             <ResultCount path={['searchRoot', 'results']} />
           </h1>
-          <ResultTable path={['searchRoot', 'results']} />
+          <style>
+            {`
+              .example-table tr:nth-child(even) {
+                  background-color: rgba(0, 0, 0, 0.5)
+              }
+              .example-table {
+                background: white;
+                color: #444;
+                border-collapse: collapse;
+              }
+              .example-table td, .example-table th {
+                padding: 5px
+              }
+              .example-table thead {
+                border-bottom: solid 2px #ccc
+              }
+            `}
+          </style>
+          <ResultTable path={['searchRoot', 'results']} fields={{
+            poster: {
+              label: 'Poster',
+              field: 'poster',
+              display: x => <img src={x} width="180" height="270" />,
+              order: 1
+            },
+            title: {
+              order: 2,
+              Cell: x => <td style={{color:'red'}} {...x} />
+            },
+            year: {
+              order: -2
+            }
+          }} infer Table={x => <table className='example-table' {...x} />} />
         </div>
       </Flex>
     </SpacedList>
