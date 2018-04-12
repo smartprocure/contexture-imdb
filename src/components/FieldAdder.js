face79b7ebab481466fb9bf406c81b83a93bd380
Samuel Greene <sgreene@smartprocure.us>
2018-04-12 22:13:06 +0200

    field adder can rely on demo controls

--- src/components/FieldAdder.js
+++ src/components/FieldAdder.js
@@ -1,62 +1,21 @@
 import _ from 'lodash/fp'
 import React from 'react'
-import * as F from 'futil-js'
-import {observer, inject} from 'mobx-react'
-import {observable} from 'mobx'
-import Modal from '../components/Modal'
-let withStateLens = state => inject(() => F.lensOf(observable(state)))
+import {observer} from 'mobx-react'
 
-let Style = () => (
-  <style>
-    {`
-.FieldPicker_Field:hover {
-  background-color: #f5f5f5;
-}
-.FieldPicker_Field {
-  cursor: pointer;
-  padding: 10px 15px;
-  border-radius: 4px
-}
-`}
-  </style>
-)
-export let FieldPicker = ({fields, onChange}) =>
-  _.map(
-    field => (
-      <div className="FieldPicker_Field">
-        <a onClick={() => onChange(field.field)}>{field.label}</a>
-      </div>
-    ),
-    fields
-  )
 
-export let FieldAdder = withStateLens({isOpen: false})(
-  ({tree, path, fields, isOpen}) => (
-    <div>
-      <Style />
-      <Modal isOpen={isOpen}>
-        <FieldPicker
-          fields={fields}
-          onChange={field => {
-            tree.add(['searchRoot', 'criteria'], {
-              key: _.uniqueId('add'),
-              field,
-              type: fields[field].typeDefault,
-            })
-            F.off(isOpen)()
-          }}
-        />
-      </Modal>
-      <button
-        style={{
-          width: '100%',
-          padding: '5px',
-          margin: '5px 0',
-          borderRadius: '5px',
-        }}
-        onClick={F.on(isOpen)}>
-        + Include Additional Filter
-      </button>
-    </div>
+export let fieldsToOptions = _.map(x => ({value: x.field, ...x}))
+
+export let FieldAdder = observer(
+  ({tree, path, fields, FieldPicker}) => (
+    <FieldPicker
+      options={fieldsToOptions(fields)}
+      onChange={field => {
+        tree.add(path, {
+          key: _.uniqueId('add'),
+          field,
+          type: fields[field].typeDefault,
+        })
+      }}
+    />
   )
 )
