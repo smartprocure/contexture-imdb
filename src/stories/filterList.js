1e0a46558eee10c68a666232f40ae375d54c5a60
Samuel Greene <sgreene@smartprocure.us>
2018-04-12 22:13:32 +0200

    leverage awaiter, schema utils, and new field adder

--- src/stories/filterList.js
+++ src/stories/filterList.js
@@ -1,6 +1,9 @@
+import _ from 'lodash/fp'
+import * as F from 'futil-js'
 import React from 'react'
-import { Provider } from 'mobx-react'
-import Contexture from '../utils/contexture'
+import {Provider} from 'mobx-react'
+import Contexture, {esClient} from '../utils/contexture'
+import {getSchemas} from '../utils/schema'
 import {
   Facet,
   Range,
@@ -8,13 +11,17 @@ import {
   ResultCount,
   DateHistogram,
 } from 'contexture-react/dist/example-types/components'
-import { Flex } from 'contexture-react/dist/example-types/Flex'
+import {Flex} from 'contexture-react/dist/example-types/Flex'
 import SpacedList from 'contexture-react/dist/example-types/SpacedList'
 import ResultTable from '../components/ResultTable'
-import { FilterList } from '../components/FilterList'
-import { FieldAdder } from '../components/FieldAdder'
+import {FilterList} from '../components/FilterList'
+import {FieldAdder} from '../components/FieldAdder'
+import Awaiter from '../components/Awaiter'
 import {applyDefaults} from '../utils/schema'
+import { observer, inject } from 'mobx-react'
+import {ModalPicker, ListGroupPicker, Button} from '../components/DemoControls'
 
+import {fromPromise} from 'mobx-utils'
 let formatYear = x => new Date(x).getFullYear() + 1
 
 let TypeMap = {
@@ -52,7 +59,7 @@ let tree = Contexture({
         {
           key: 'searchActors',
           type: 'facet',
-          field: 'actors'
+          field: 'actors',
         },
       ],
     },
@@ -72,72 +79,91 @@ let tree = Contexture({
   ],
 })
 
+// TODO: example story book for field picker only
 let schema = applyDefaults({
   directors: {
     typeDefault: 'facet',
   },
   runtimeMinutes: {
-    typeDefault: 'number'
-  }
+    typeDefault: 'number',
+  },
 })
+let schemas = fromPromise(getSchemas(esClient))
+
+// Pre apply some props
+let FieldPicker = inject(() => ({
+  Select: ListGroupPicker,
+  Button,
+  label: '+ Include Additional Filter'
+}))(ModalPicker)
+
 export default () => (
-  <Provider tree={tree}>
-    <SpacedList>
-      <Query path={['searchRoot', 'searchQuery']} />
-      <Flex>
-        <div style={{ flex: 1 }}>
-          <FilterList
-            path={['searchRoot', 'criteria']}
-            typeComponents={TypeMap}
-          />
-          <FieldAdder fields={schema} path={['searchRoot', 'criteria']} tree={tree} />
-        </div>
-        <div style={{ flex: 4 }}>
-          <ResultCount path={['searchRoot', 'results']} />
-          <DateHistogram
-            path={['searchRoot', 'releases']}
-            format={formatYear}
-          />
-          <style>
-            {`
-              .example-table tr:nth-child(even) {
-                background-color: rgba(0, 0, 0, 0.5)
-              }
-              .example-table {
-                background: white;
-                color: #444;
-                border-collapse: collapse;
-              }
-              .example-table td, .example-table th {
-                padding: 5px
-              }
-              .example-table thead {
-                border-bottom: solid 2px #ccc
-              }
-            `}
-          </style>
-          <ResultTable
-            path={['searchRoot', 'results']}
-            fields={{
-              poster: {
-                label: 'Poster',
-                field: 'poster',
-                display: x => <img src={x} width="180" height="270" />,
-                order: 1,
-              },
-              title: {
-                order: 2,
-                Cell: x => <td style={{ color: 'red' }} {...x} />,
-              },
-              year: {
-                order: -2,
-              },
-            }}
-            infer
-            Table={x => <table className="example-table" {...x} />}
-          />
-        </div>
-      </Flex>
-    </SpacedList>
-  </Provider>
+  <Awaiter promise={schemas}>
+    {schemas => (
+      <Provider tree={tree}>
+        <SpacedList>
+          <Query path={['searchRoot', 'searchQuery']} />
+          <Flex>
+            <div style={{flex: 1}}>
+              <FilterList
+                path={['searchRoot', 'criteria']}
+                typeComponents={TypeMap}
+              />
+              <FieldAdder
+                tree={tree}
+                path={['searchRoot', 'criteria']}
+                fields={schemas.movies.fields}
+                FieldPicker={FieldPicker}
+              />
+            </div>
+            <div style={{flex: 4}}>
+              <ResultCount path={['searchRoot', 'results']} />
+              <DateHistogram
+                path={['searchRoot', 'releases']}
+                format={formatYear}
+              />
+              <style>
+                {`
+                  .example-table tr:nth-child(even) {
+                    background-color: rgba(0, 0, 0, 0.5)
+                  }
+                  .example-table {
+                    background: white;
+                    color: #444;
+                    border-collapse: collapse;
+                  }
+                  .example-table td, .example-table th {
+                    padding: 5px
+                  }
+                  .example-table thead {
+                    border-bottom: solid 2px #ccc
+                  }
+                `}
+              </style>
+              <ResultTable
+                path={['searchRoot', 'results']}
+                fields={{
+                  poster: {
+                    label: 'Poster',
+                    field: 'poster',
+                    display: x => <img src={x} width="180" height="270" />,
+                    order: 1,
+                  },
+                  title: {
+                    order: 2,
+                    Cell: x => <td style={{color: 'red'}} {...x} />,
+                  },
+                  year: {
+                    order: -2,
+                  },
+                }}
+                infer
+                Table={x => <table className="example-table" {...x} />}
+              />
+            </div>
+          </Flex>
+        </SpacedList>
+      </Provider>
+    )}
+  </Awaiter>
 )
